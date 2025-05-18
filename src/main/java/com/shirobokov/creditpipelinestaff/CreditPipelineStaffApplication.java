a3e5c56b39b150c77919c92e6a643bb7006685f2
AntonShirobokov <shirobokov.anton2016@gmail.com>
2025-05-18 14:06:04 +0400

    Создание проекта

---
+++ src/main/java/com/shirobokov/creditpipelinestaff/CreditPipelineStaffApplication.java
@@ -0,0 +1,13 @@
+package com.shirobokov.creditpipelinestaff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditPipelineStaffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditPipelineStaffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shirobokov/creditpipelinestaff/CreditPipelineStaffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shirobokov.creditpipelinestaff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditPipelineStaffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
